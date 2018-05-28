3368475198dea4aa83154527cbfd528dfd9312f6
Chandu J S <chandujs@live.com>
2018-05-29 00:06:06 +0530

    about content & minor style changes

--- js/app.js
+++ js/app.js
@@ -1,55 +1,55 @@
 // App Theme JavaScript
 
-(function($) {
-    "use strict"; // Start of use strict
+(function ($) {
+  "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('.page-scroll a').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 51
+  });
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function () {
+    $('.navbar-toggle:visible').click();
+  });
+
+  // Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 100
+    }
+  })
+
+  // Floating label headings for the contact form
+  $(function () {
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function () {
+      $(this).removeClass("floating-label-form-group-with-focus");
     });
+  });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
-
-    // Floating label headings for the contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
-    });
-
-    $('[data-tooltip]').tooltip(); 
+  $('[data-tooltip]').tooltip();
 
 })(jQuery); // End of use strict
 
-angular.module('cujs', []).controller('main', ['$scope', '$http', function($scope, $http) {
-    $scope.portfolio = null
-    $http.get('portfolio.json').then(function(response){
-        $scope.portfolio = response.data;
-    }).catch(function(error){
-        console.error(error);
-    });
+angular.module('cujs', []).controller('main', ['$scope', '$http', function ($scope, $http) {
+  $scope.portfolio = null;
+  $http.get('portfolio.json').then(function (response) {
+    $scope.portfolio = response.data;
+  }).catch(function (error) {
+    console.error(error);
+  });
 }]);
