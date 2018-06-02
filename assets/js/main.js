0584d35a8ea4cdc2eb119e0af5d4f8260562ccb2
Chandu J S <chandujs@live.com>
2018-06-03 00:25:39 +0530

    works & index page styles finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 ---
 ---
-(() => {
-})()
+window.dataLayer = window.dataLayer || []
+const gtag = function () {
+  window.dataLayer.push(arguments)
+}
+gtag('js', new Date())
+gtag('config', 'UA-118450969-1')
