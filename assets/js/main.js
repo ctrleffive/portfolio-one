c2422c0acd232ed53c29aaa5fdfedf362faf99b6
Chandu J S <chandujs@live.com>
2018-06-05 21:48:38 +0530

    content adding

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
 ---
 ---
+// Google Analytics
 window.dataLayer = window.dataLayer || []
 const gtag = function () {
   window.dataLayer.push(arguments)
 }
 gtag('js', new Date())
 gtag('config', 'UA-118450969-1')
+
+// Smooth navigation
+window.onload = () => {
+  setTimeout(() => {
+    $('body').addClass('ok')
+  }, 1000)
+}
