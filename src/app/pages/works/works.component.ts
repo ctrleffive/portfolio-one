e33bfa108e13d9366f15862a5bbef211d4cc948c
Chandu J S <app.coder@outlook.in>
2019-11-01 21:14:44 +0400

    styles seperated, other modules added

---
+++ src/app/pages/works/works.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-works',
+  templateUrl: './works.component.html',
+  styleUrls: ['./works.component.scss']
+})
+export class WorksComponent {
+  constructor() { }
+}
