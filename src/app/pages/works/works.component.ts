4454e4d320c330022f7e39b39e2cea572049d9a8
Chandu J S <dev@dotsadvertising.net>
2019-11-02 16:58:23 +0400

    works listing page styled

--- src/app/pages/works/works.component.ts
+++ src/app/pages/works/works.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { Work } from 'src/app/models/work'
+import { NgxMasonryOptions } from 'ngx-masonry'
 
 @Component({
   selector: 'app-works',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core'
   styleUrls: ['./works.component.scss']
 })
 export class WorksComponent {
-  constructor() { }
+  public works: Array<Work>
+  public masonryOptions: NgxMasonryOptions
+
+  constructor() {
+    this.masonryOptions = {
+
+    }
+    this.works = [
+      new Work({ slug: 'work-1' }),
+      new Work({ slug: 'work-2' }),
+      new Work({ slug: 'work-3' }),
+      new Work({ slug: 'work-4' }),
+      new Work({ slug: 'work-5' }),
+      new Work({ slug: 'work-6' }),
+      new Work({ slug: 'work-7' }),
+      new Work({ slug: 'work-8' }),
+    ]
+  }
 }
