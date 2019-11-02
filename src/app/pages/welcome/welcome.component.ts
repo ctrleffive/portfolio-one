e87b1cc557fe4792424b107c059f363ed7d16ee8
Chandu J S <dev@dotsadvertising.net>
2019-11-02 14:43:24 +0400

    welcome text left alignment

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core'
+import { Component, HostBinding, OnInit } from '@angular/core'
+import { SafeStyle, DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
-  constructor() { }
+export class WelcomeComponent implements OnInit {
+  @HostBinding('style.background-image') public background: SafeStyle
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  private setBg(): void {
+    const num: number = Math.ceil((Math.random() * (10 - 1) + 1))
+    this.background = this.sanitizer.bypassSecurityTrustStyle(`url('assets/images/bgs/welcome/${num}.svg')`)
+  }
+  ngOnInit(): void {
+    this.setBg()
+  }
 }
