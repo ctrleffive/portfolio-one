e96da32f19edc3048209d868b361d427b4d1d16f
Chandu J S <dev@dotsadvertising.net>
2019-11-02 20:25:10 +0400

    works single finished, works images sorted

--- src/app/models/work.ts
+++ src/app/models/work.ts
@@ -1,20 +1,36 @@
 export class Work {
   public slug: string
   public title: string
+  public link: string
+  public type: string
+  public screens: number
+  public linkLabel: string
   public tags: Array<string>
 
   constructor({
     slug,
     title,
-    tags
+    link,
+    type,
+    tags,
+    screens,
+    linkLabel
   }: {
     slug?: string,
-    title?: string
-    tags?: Array<string>
+    type?: string,
+    link?: string,
+    title?: string,
+    tags?: Array<string>,
+    screens?: number,
+    linkLabel?: string
   }) {
     if (slug) { this.slug = slug }
     if (tags) { this.tags = tags}
+    if (type) { this.type = type}
+    if (link) { this.link = link}
     if (title) { this.title = title}
+    if (screens) { this.screens = screens}
+    if (linkLabel) { this.linkLabel = linkLabel}
   }
 
   /**
