b0591cbaa52d6247cbe5984089ba155a73869c79
Chandu J S <app.coder@outlook.in>
2019-12-18 23:10:25 +0400

    data set moved

--- src/app/pages/work-single/work-single.component.ts
+++ src/app/pages/work-single/work-single.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http'
 import { ActivatedRoute, Router } from '@angular/router'
 import { Work } from 'src/app/models/work'
 import { SystemService } from 'src/app/shared/system.service'
+import { WorksData } from 'src/app/data/works'
 
 @Component({
   selector: 'app-work-single',
@@ -17,20 +18,15 @@ export class WorkSingleComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private systemService: SystemService
-    ) {}
+  ) { }
 
-  private async getData(): Promise<void> {
-    this.systemService.loader = true
-    const slug: string = this.route.snapshot.paramMap.get('slug')
-    const apiResponse: any = await this.http.get('/assets/data/works.json').toPromise()
-    const works: Array<Work> = Work.fromJsonList(apiResponse).filter(item => item.slug === slug)
-    if (works.length) {
-      this.work = works[0]
-      this.systemService.appTitle = `${this.work.title} | ${this.work.type}`
-    } else {
+  private getData(): void {
+    try {
+      const slug: string = this.route.snapshot.paramMap.get('slug')
+      this.work = WorksData.single(slug)
+    } catch (error) {
       this.router.navigate(['/works'])
     }
-    this.systemService.loader = false
   }
 
   ngOnInit(): void {
--- src/app/pages/works/works.component.ts
+++ src/app/pages/works/works.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Work } from 'src/app/models/work'
 import { SystemService } from 'src/app/shared/system.service'
+import { WorksData } from 'src/app/data/works'
 
 @Component({
   selector: 'app-works',
@@ -15,15 +16,8 @@ export class WorksComponent implements OnInit {
     this.works = []
   }
 
-  private async getData(): Promise<void> {
-    this.systemService.loader = true
-    const apiResponse: any = await this.http.get('/assets/data/works.json').toPromise()
-    this.works = Work.fromJsonList(apiResponse)
-    this.systemService.loader = false
-  }
-
   ngOnInit(): void {
-    this.getData()
+    this.works = WorksData.all
     this.systemService.appTitle = 'Portfolio of Chandu'
     this.systemService.setBg('works.svg')
   }
