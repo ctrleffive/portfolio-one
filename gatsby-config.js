cfbdb494966128bdd0d5234d4ad22779b00ebdc5
Chandu J S <app.coder@outlook.in>
2020-01-11 23:44:44 +0400

    styles updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [],
+        fonts: [`Raleway`],
         display: 'swap',
       },
     },
