03147fcef010543fd62eec9b9158b47ef161a488
Chandu J S <app.coder@outlook.in>
2020-01-11 22:43:09 +0400

    gatsby added

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  hmr: false,
-  analyticsId: 'G-97JFNZHF9C',
-  disqusId: 'ctrleffive'
-}
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const IndexPage = props => <h1>Hi! 👋</h1>
+
+export default IndexPage
