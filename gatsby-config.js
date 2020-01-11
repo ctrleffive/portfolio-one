a327e1bd796e0cca8eae704805b1a1d38926c61f
Chandu J S <app.coder@outlook.in>
2020-01-11 22:43:09 +0400

    gatsby added

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [],
+        display: 'swap',
+      },
+    },
+  ],
+}
--- src/environments/environment.hmr.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  hmr: true,
-  analyticsId: 'G-97JFNZHF9C',
-  disqusId: 'ctrleffive'
-}
