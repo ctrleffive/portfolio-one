84656beffa47354e8a2dfd1cbcdb55bf139b92a6
Chandu J S <app.coder@outlook.in>
2020-02-04 23:13:18 +0400

    blog section set

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,16 +5,45 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `Chandu J S 💻 Full Stack Developer`,
+    social: [
+      {
+        service: `DEV Community`,
+        url: `https://dev.to/ctrleffive`,
+      },
+      {
+        service: `GitHub`,
+        url: `https://github.com/ctrleffive`,
+      },
+      {
+        service: `Instagram`,
+        url: `https://instagram.com/ctrleffive`,
+      },
+      {
+        service: `LinkedIn`,
+        url: `https://linkedin.com/in/ctrleffive`,
+      },
+      {
+        service: `Twitter`,
+        url: `https://twitter.com/ctrleffive`,
+      },
+    ],
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Raleway\:400,700`
-        ],
+        fonts: [`Raleway\:400,700`],
         display: 'swap',
+      },
+    },
+    {
+      resolve: `gatsby-source-dev`,
+      options: {
+        username: 'ctrleffive',
       },
     },
   ],
--- src/layouts/wrap.js
+++ src/layouts/wrap.js
@@ -1,7 +1,8 @@
 /** @jsx jsx */
 
-import { Link } from 'gatsby'
+import { Link, StaticQuery } from 'gatsby'
 import { Component } from 'react'
+import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 import { Global, css, jsx } from '@emotion/core'
 import { GlobalStyles, ColorsAdvanced, Colors } from '../styles/main'
@@ -27,273 +28,272 @@ export default class Wrap extends Component {
     `
 
     return (
-      <div
-        className={this.props.lightsOn ? 'lights-on' : ''}
-        css={css`
-          .side-bar {
-            position: fixed;
-            top: 0;
-            bottom: 0;
-            width: 10rem;
-            padding: 2rem;
-            display: flex;
-            flex-wrap: wrap;
-            flex-direction: column;
-            justify-content: space-between;
-            z-index: 2;
-
-            @media screen and (max-width: 500px) {
-              position: initial;
-              display: block;
-              width: 100%;
-              padding-bottom: 0;
-            }
-          }
-        `}>
-        <Helmet
-          base="/"
-          defaultTitle={
-            this.props.seoTitle || 'Chandu J S 💻 Full Stack Developer'
-          }
-        />
-        <Global styles={styles} />
-        <div
-          className="side-bar"
-          css={css`
-            left: 0;
-          `}>
-          <Link
-            to="/"
-            css={css`
-              letter-spacing: 0.5rem;
-              text-transform: uppercase;
-              font-size: 0.8rem;
-              line-height: 2rem;
-              position: relative;
-              top: -0.6rem;
-              > div {
-                @media screen and (max-width: 500px) {
-                  display: inline;
-                  white-space: nowrap;
+      <StaticQuery
+        query={graphql`
+          query {
+            site {
+              siteMetadata {
+                title
+                social {
+                  service
+                  url
                 }
               }
-            `}>
-            <div className="mr-3">Chandu</div>
-            <div>J S</div>
-          </Link>
-          <a href="#" className="contact-button"></a>
-        </div>
-        <div
-          className="side-bar text-right"
-          css={css`
-            right: 0;
-            width: 8rem;
-            @media screen and (max-width: 500px) {
-              position: initial;
-              top: 0;
-              right: 0;
-              bottom: initial;
-              width: initial;
-              padding-top: 0;
-              text-align: left !important;
-              margin-bottom: -1rem;
             }
-          `}>
-          <nav
-            css={css`
-              a {
-                font-weight: bold;
-                text-transform: lowercase;
-                font-size: 1.4rem;
-                position: relative;
-                transition-duration: 0.2s;
-                transform: translateY(0.1rem);
-                display: inline-block;
-                margin-bottom: 2rem;
-                margin-top: -1rem;
-                &:before {
-                  content: '';
-                  border-bottom: 2px solid;
-                  position: absolute;
-                  left: 0;
-                  right: 0;
-                  bottom: 0;
-                  color: ${Colors.accent};
-                  transition-duration: 0.2s;
-                  opacity: 0;
-                  transform: translateY(-0.7rem);
-                  transition: color 0.5s;
-                  .lights-on & {
-                    color: ${ColorsAdvanced.accentLightsOn};
-                  }
-                }
-                &.active {
-                  color: ${Colors.accent};
-                  .lights-on & {
-                    color: ${ColorsAdvanced.accentLightsOn};
-                  }
-                }
-                &:hover {
-                  transform: translateY(0rem);
-                  color: #fff;
-                  .lights-on & {
-                    color: #000;
-                  }
-                  &:before {
-                    opacity: 1;
-                    transform: translateY(0rem);
-                  }
-                }
-                @media screen and (max-width: 500px) {
-                  margin-right: 2rem;
-                  margin-bottom: 1rem;
-                  margin-top: 0rem;
-                }
-              }
-            `}>
-            {/* <Link activeClassName="active" to="/works">
-              Works
-            </Link> */}
-            <Link activeClassName="active" to="/blog">
-              Blog
-            </Link>
-            <Link activeClassName="active" to="/about">
-              About
-            </Link>
-            <Link activeClassName="active" to="/uses">
-              Uses
-            </Link>
-          </nav>
+          }
+        `}
+        render={({ site }) => (
           <div
+            className={this.props.lightsOn ? 'lights-on' : ''}
             css={css`
-              right: 2rem;
-              bottom: 2rem;
-              position: absolute;
-              @media screen and (max-width: 500px) {
-                position: absolute;
-                left: 0;
-                right: 0;
-                bottom: initial;
-                -webkit-box-ordinal-group: 2;
-                order: 1;
-                width: 100%;
-                display: block;
-                padding-left: 1.6rem;
-              }
-              a {
-                display: block;
-                width: 2.3rem;
-                height: 2.3rem;
-                margin-top: 1rem;
-                background-position: center;
-                background-size: 1.5rem;
-                background-repeat: no-repeat;
-                opacity: 0.5;
-                transition-duration: 0.2s;
+              .side-bar {
+                position: fixed;
+                top: 0;
+                bottom: 0;
+                width: 10rem;
+                padding: 2rem;
+                display: flex;
+                flex-wrap: wrap;
+                flex-direction: column;
+                justify-content: space-between;
+                z-index: 2;
+
                 @media screen and (max-width: 500px) {
-                  display: inline-block;
-                }
-                .lights-on & {
-                  opacity: 0.7;
-                  filter: invert(1);
-                }
-                &:last-child {
-                  margin-bottom: 0;
-                }
-                &:hover {
-                  opacity: 1;
-                }
-                &[alt='DEV Community'] {
-                  background-image: url(${devIcon});
-                }
-                &[alt='GitHub'] {
-                  background-image: url(${githubIcon});
-                }
-                &[alt='Instagram'] {
-                  background-image: url(${instagramIcon});
-                }
-                &[alt='LinkedIn'] {
-                  background-image: url(${linkedinIcon});
-                }
-                &[alt='Twitter'] {
-                  background-image: url(${twitterIcon});
+                  position: initial;
+                  display: block;
+                  width: 100%;
+                  padding-bottom: 0;
                 }
               }
             `}>
-            <a
-              href="https://dev.to/ctrleffive"
-              target="_blank"
-              className="social-icon"
-              rel="noreferrer"
-              alt="DEV Community"></a>
-            <a
-              href="https://github.com/ctrleffive"
-              target="_blank"
-              className="social-icon"
-              rel="noreferrer"
-              alt="GitHub"></a>
-            <a
-              href="https://instagram.com/ctrleffive"
-              target="_blank"
-              className="social-icon"
-              rel="noreferrer"
-              alt="Instagram"></a>
-            <a
-              href="https://linkedin.com/in/ctrleffive"
-              target="_blank"
-              className="social-icon"
-              rel="noreferrer"
-              alt="LinkedIn"></a>
-            <a
-              href="https://twitter.com/ctrleffive"
-              target="_blank"
-              className="social-icon"
-              rel="noreferrer"
-              alt="Twitter"></a>
+            <Helmet
+              base="/"
+              defaultTitle={this.props.seoTitle || site.siteMetadata.title}
+            />
+            <Global styles={styles} />
+            <div
+              className="side-bar"
+              css={css`
+                left: 0;
+              `}>
+              <Link
+                to="/"
+                css={css`
+                  letter-spacing: 0.5rem;
+                  text-transform: uppercase;
+                  font-size: 0.8rem;
+                  line-height: 2rem;
+                  position: relative;
+                  top: -0.6rem;
+                  > div {
+                    @media screen and (max-width: 500px) {
+                      display: inline;
+                      white-space: nowrap;
+                    }
+                  }
+                `}>
+                <div className="mr-3">Chandu</div>
+                <div>J S</div>
+              </Link>
+              <a href="#" className="contact-button"></a>
+            </div>
+            <div
+              className="side-bar text-right"
+              css={css`
+                right: 0;
+                width: 8rem;
+                @media screen and (max-width: 500px) {
+                  position: initial;
+                  top: 0;
+                  right: 0;
+                  bottom: initial;
+                  width: initial;
+                  padding-top: 0;
+                  text-align: left !important;
+                  margin-bottom: -1rem;
+                }
+              `}>
+              <nav
+                css={css`
+                  a {
+                    font-weight: bold;
+                    text-transform: lowercase;
+                    font-size: 1.4rem;
+                    position: relative;
+                    transition-duration: 0.2s;
+                    transform: translateY(0.1rem);
+                    display: inline-block;
+                    margin-bottom: 2rem;
+                    margin-top: -1rem;
+                    &:before {
+                      content: '';
+                      border-bottom: 2px solid;
+                      position: absolute;
+                      left: 0;
+                      right: 0;
+                      bottom: 0;
+                      color: ${Colors.accent};
+                      transition-duration: 0.2s;
+                      opacity: 0;
+                      transform: translateY(-0.7rem);
+                      transition: color 0.5s;
+                      .lights-on & {
+                        color: ${ColorsAdvanced.accentLightsOn};
+                      }
+                    }
+                    &.active {
+                      color: ${Colors.accent};
+                      .lights-on & {
+                        color: ${ColorsAdvanced.accentLightsOn};
+                      }
+                    }
+                    &:hover {
+                      transform: translateY(0rem);
+                      color: #fff;
+                      .lights-on & {
+                        color: #000;
+                      }
+                      &:before {
+                        opacity: 1;
+                        transform: translateY(0rem);
+                      }
+                    }
+                    @media screen and (max-width: 500px) {
+                      margin-right: 2rem;
+                      margin-bottom: 1rem;
+                      margin-top: 0rem;
+                    }
+                  }
+                `}>
+                <Link activeClassName="active" to="/works">
+                  Works
+                </Link>
+                <Link
+                  className={this.props.isBlogPage ? 'active' : ''}
+                  activeClassName="active"
+                  to="/blog">
+                  Blog
+                </Link>
+                <Link activeClassName="active" to="/about">
+                  About
+                </Link>
+                <Link activeClassName="active" to="/uses">
+                  Uses
+                </Link>
+              </nav>
+              <div
+                css={css`
+                  right: 2rem;
+                  bottom: 2rem;
+                  position: absolute;
+                  @media screen and (max-width: 500px) {
+                    position: absolute;
+                    left: 0;
+                    right: 0;
+                    bottom: initial;
+                    -webkit-box-ordinal-group: 2;
+                    order: 1;
+                    width: 100%;
+                    display: block;
+                    padding-left: 1.6rem;
+                  }
+                  a {
+                    display: block;
+                    width: 2.3rem;
+                    height: 2.3rem;
+                    margin-top: 1rem;
+                    background-position: center;
+                    background-size: 1.5rem;
+                    background-repeat: no-repeat;
+                    opacity: 0.5;
+                    transition-duration: 0.2s;
+                    font-size: 0;
+                    @media screen and (max-width: 500px) {
+                      display: inline-block;
+                    }
+                    .lights-on & {
+                      opacity: 0.7;
+                      filter: invert(1);
+                    }
+                    &:last-child {
+                      margin-bottom: 0;
+                    }
+                    &:hover {
+                      opacity: 1;
+                    }
+                    &[alt='DEV Community'] {
+                      background-image: url(${devIcon});
+                    }
+                    &[alt='GitHub'] {
+                      background-image: url(${githubIcon});
+                    }
+                    &[alt='Instagram'] {
+                      background-image: url(${instagramIcon});
+                    }
+                    &[alt='LinkedIn'] {
+                      background-image: url(${linkedinIcon});
+                    }
+                    &[alt='Twitter'] {
+                      background-image: url(${twitterIcon});
+                    }
+                  }
+                `}>
+                {site.siteMetadata.social.map(item => (
+                  <a
+                    href={item.url}
+                    target="_blank"
+                    className="social-icon"
+                    rel="noreferrer"
+                    alt={item.service}>
+                    {item.service}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div
+              css={css`
+                animation: fadeIn 0.2s ease-in-out;
+
+                @keyframes fadeIn {
+                  0% {
+                    opacity: 0;
+                    margin-top: 0.5rem;
+                  }
+                  100% {
+                    opacity: 1;
+                    margin-top: 0;
+                  }
+                }
+              `}>
+              {this.props.children}
+              <img
+                src={this.props.pageBg}
+                css={css`
+                  top: 50%;
+                  left: 50%;
+                  z-index: -1;
+                  opacity: 0.1;
+                  position: fixed;
+                  transition-duration: 0.3s;
+                  transform: translate(calc(50% - 35rem), -50%);
+                  max-height: 100%;
+                  &:hover {
+                    opacity: 0.3;
+                  }
+
+                  @media screen and (max-width: 1000px) {
+                    transform: translate(-50%, -50%);
+                  }
+                  @media screen and (max-width: 500px) {
+                    transform: translate(-25%, -50%);
+                  }
+                `}
+                alt=""
+              />
+            </div>
           </div>
-        </div>
-        <div
-          css={css`
-            animation: fadeIn 0.2s ease-in-out;
-
-            @keyframes fadeIn {
-              0% {
-                opacity: 0;
-                margin-top: 0.5rem;
-              }
-              100% {
-                opacity: 1;
-                margin-top: 0;
-              }
-            }
-          `}>
-          {this.props.children}
-          <img
-            src={this.props.pageBg}
-            css={css`
-              top: 50%;
-              left: 50%;
-              z-index: -1;
-              opacity: 0.1;
-              position: fixed;
-              transition-duration: 0.3s;
-              transform: translate(calc(50% - 35rem), -50%);
-              max-height: 100%;
-              &:hover {
-                opacity: 0.3;
-              }
-
-              @media screen and (max-width: 1000px) {
-                transform: translate(-50%, -50%);
-              }
-              @media screen and (max-width: 500px) {
-                transform: translate(-25%, -50%);
-              }
-            `}
-            alt=""
-          />
-        </div>
-      </div>
+        )}
+      />
     )
   }
 }
