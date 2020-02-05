321f6d00e93b4d68e74354f1928f06be1116d8d0
Chandu J S <app.coder@outlook.in>
2020-02-06 00:52:28 +0400

    svg inline added

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -1,12 +1,12 @@
 import Wrap from '../layouts/wrap'
 import React, { Component } from 'react'
 
-import pageBg from '../assets/images/bgs/uses.svg'
+import PageBg from '../assets/images/bgs/uses.svg'
 
 export default class UsesPage extends Component {
   render = () => {
     return (
-      <Wrap pageBg={pageBg} seoTitle="Uses | Chandu J S">
+      <Wrap pageBg={<PageBg />} seoTitle="Uses | Chandu J S">
         <div className="content-wrap">
           <div className="h1 font-weight-bold mb-5">
             <span className="high">Uses</span>
--- src/templates/works.js
+++ src/templates/works.js
@@ -1,12 +1,12 @@
 import Wrap from '../layouts/wrap'
 import React, { Component } from 'react'
 
-import pageBg from '../assets/images/bgs/works.svg'
+import PageBg from '../assets/images/bgs/works.svg'
 
 export default class WorksPage extends Component {
   render = () => {
     return (
-      <Wrap pageBg={pageBg} seoTitle="Works of Chandu | Chandu J S">
+      <Wrap pageBg={<PageBg />} seoTitle="Works of Chandu | Chandu J S">
         <div className="content-wrap">
           <div className="h1 font-weight-bold mb-5">
             My <span className="high">Works</span>
