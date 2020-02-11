4e8dea9889fe0b0707c7e0a89d2c6251f91e91ff
Chandu J S <app.coder@outlook.in>
2020-02-11 06:43:54 +0400

    uses template page set

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -1,7 +1,10 @@
 /** @jsx jsx */
 
+import { StaticQuery, graphql } from 'gatsby'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import { css, jsx } from '@emotion/core'
 import Wrap from '../layouts/wrap'
+import { GlobalStyles, ColorsAdvanced, Colors } from '../styles/main'
 import { Component } from 'react'
 
 import PageBg from '../assets/images/bgs/uses.svg'
@@ -10,14 +13,33 @@ export default class UsesPage extends Component {
   render = () => {
     return (
       <Wrap pageBg={<PageBg />} seoTitle="Uses | Chandu J S">
-        <div className="content-wrap">
+        <div
+          css={css`
+            li {
+              line-height: 2;
+              &:hover > span {
+                color: ${Colors.accent};
+                .lights-on & {
+                  color: ${ColorsAdvanced.accentLightsOn};
+                }
+              }
+            }
+          `}
+          className="content-wrap">
           <div className="h1 font-weight-bold mb-3">
             <span className="high">Uses</span>
             <span className="blinker">.</span>
             <br />
           </div>
-          <div className="h5">
-            Curious to know what I use on a daily basis? Just check it out here
+          <div
+            className="h5 mb-4 pb-4"
+            css={css`
+              line-height: 1.3;
+            `}>
+            Curious to know what I use on a daily basis? Just check it out here.{' '}
+            <br />
+            My setup will change time to time. But I will try to keep this
+            uptodate.
             <span
               css={css`
                 margin-left: 5px;
@@ -27,6 +49,73 @@ export default class UsesPage extends Component {
               😃
             </span>
           </div>
+
+          <StaticQuery
+            query={graphql`
+              query {
+                site {
+                  siteMetadata {
+                    uses {
+                      title
+                      items {
+                        emoji
+                        name
+                        description
+                        link
+                      }
+                      description
+                    }
+                  }
+                }
+              }
+            `}
+            render={({ site }) => (
+              <div className="row">
+                {site.siteMetadata.uses.map(section => (
+                  <div className="col-md-6 mb-4 mt-1">
+                    <div className="h3 mb-2">{section.title}</div>
+                    <p className={section.description ? `mb-4` : `pb-0`}>
+                      {section.description}
+                    </p>
+                    <ul className="no-bullets">
+                      {section.items.map(item => (
+                        <li>
+                          <span role="img" aria-label="smile" className="mr-2">
+                            {item.emoji}
+                          </span>{' '}
+                          <span
+                            css={css`
+                              font-weight: ${item.description
+                                ? 'bold'
+                                : 'inherit'};
+                            `}>
+                            {item.link ? (
+                              <OutboundLink href={item.link}>
+                                {item.name}
+                              </OutboundLink>
+                            ) : (
+                              item.name
+                            )}
+                          </span>
+                          {item.description ? (
+                            <div
+                              className="ml-4 pl-2 mb-3"
+                              css={css`
+                                line-height: 1.5;
+                              `}>
+                              {item.description}
+                            </div>
+                          ) : (
+                            ''
+                          )}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            )}
+          />
         </div>
       </Wrap>
     )
