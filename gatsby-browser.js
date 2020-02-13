b2a6d635a30a3d80a51b446032179e5c2e5a26cb
Chandu J S <app.coder@outlook.in>
2020-02-13 15:03:26 +0400

    pwa features added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,12 @@
 import './src/assets/css/bootstrap.css'
 import './src/assets/css/base.css'
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `Website has been updated. Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
